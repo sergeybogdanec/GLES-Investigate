8427152ee023d5a7a6775a8d674ffad1e8883bd9
Sergey <sweid531@gmail.com>
2021-07-20 17:26:04 +0300

    Rework

--- app/src/main/java/com/sergeybogdanec/gles/investigate/renderer/MyGLRenderer.kt
+++ app/src/main/java/com/sergeybogdanec/gles/investigate/renderer/MyGLRenderer.kt
@@ -5,49 +5,15 @@ import android.graphics.Color
 import android.opengl.GLES31
 import android.opengl.GLSurfaceView
 import android.util.Log
-import com.sergeybogdanec.gles.investigate.shapes.MyGLTriangle
 import java.io.InputStreamReader
 import javax.microedition.khronos.egl.EGLConfig
 import javax.microedition.khronos.opengles.GL10
 
 class MyGLRenderer(private val context: Context) : GLSurfaceView.Renderer {
 
-    private lateinit var triangle: MyGLTriangle
-
     private var programObject: Int = 0
 
     override fun onSurfaceCreated(unused: GL10, config: EGLConfig?) {
-        triangle = MyGLTriangle(
-            vertices = floatArrayOf(
-                0f, .62f, 0f,
-                -.5f, -.31f, 0f,
-                .5f, -.31f, 0f
-            ),
-            color = Color . valueOf (Color.MAGENTA)
-        )
-
-        val vertexShader = loadShader(GLES31.GL_VERTEX_SHADER, "vertex_shader.glsl")
-        val fragmentShader = loadShader(GLES31.GL_FRAGMENT_SHADER, "fragment_shader.glsl")
-        val program = GLES31.glCreateProgram()
-
-        if (program == 0) return
-
-        GLES31.glAttachShader(program, vertexShader)
-        GLES31.glAttachShader(program, fragmentShader)
-
-        GLES31.glLinkProgram(program)
-
-        val linked = IntArray(1)
-
-        GLES31.glGetProgramiv(program, GLES31.GL_LINK_STATUS, linked, 0)
-
-        if (linked[0] == 0) {
-            Log.e("Sergey", GLES31.glGetProgramInfoLog(program))
-            GLES31.glDeleteProgram(program)
-            return
-        }
-
-        programObject = program
         GLES31.glClearColor(0f, 0f, 0f, 1f)
     }
 
@@ -71,13 +37,5 @@ class MyGLRenderer(private val context: Context) : GLSurfaceView.Renderer {
         GLES31.glDisableVertexAttribArray(0)
     }
 
-    private fun loadShader(type: Int, shaderName: String): Int {
-        return GLES31.glCreateShader(type).also { shader ->
-            val shaderCode = InputStreamReader(context.assets.open(shaderName))
-                .readLines()
-                .joinToString("\n")
-            GLES31.glShaderSource(shader, shaderCode)
-            GLES31.glCompileShader(shader)
-        }
-    }
+
 }
---
+++ app/src/main/java/com/sergeybogdanec/gles/investigate/shaders/Shader.kt
@@ -0,0 +1,56 @@
+package com.sergeybogdanec.gles.investigate.shaders
+
+import android.content.res.AssetManager
+import android.opengl.GLES31
+import android.util.Log
+import java.io.InputStreamReader
+import kotlin.IllegalStateException
+
+abstract class Shader(
+    vertexShaderAssetName: String,
+    fragmentShaderAssetName: String,
+    private val assets: AssetManager,
+) {
+
+    private var vertexShaderId: Int = 0
+    private var fragmentShaderId: Int = 0
+
+    var programId: Int = 0
+
+    init {
+        vertexShaderId = loadShader(GLES31.GL_VERTEX_SHADER, vertexShaderAssetName)
+        fragmentShaderId = loadShader(GLES31.GL_FRAGMENT_SHADER, fragmentShaderAssetName)
+
+        val program = GLES31.glCreateProgram()
+
+        if (program == 0) throw IllegalStateException("Can't create program!")
+
+        GLES31.glAttachShader(program, vertexShaderId)
+        GLES31.glAttachShader(program, fragmentShaderId)
+
+        GLES31.glLinkProgram(program)
+
+        val linked = IntArray(1)
+
+        GLES31.glGetProgramiv(program, GLES31.GL_LINK_STATUS, linked, 0)
+
+        if (linked[0] == 0) {
+            Log.e("Sergey", GLES31.glGetProgramInfoLog(program))
+            GLES31.glDeleteProgram(program)
+            throw IllegalStateException("Program is not linked!")
+        }
+
+        programId = program
+    }
+
+    private fun loadShader(type: Int, shaderName: String): Int {
+        return GLES31.glCreateShader(type).also { shader ->
+            val shaderCode = InputStreamReader(assets.open(shaderName))
+                .readLines()
+                .joinToString("\n")
+            GLES31.glShaderSource(shader, shaderCode)
+            GLES31.glCompileShader(shader)
+        }
+    }
+
+}
